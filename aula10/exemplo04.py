c24305541c74923a3bac789c75c9a1e8adb549b9
vlv1911 <vagnerlvalle@gmail.com>
2026-05-05 19:37:57 -0300

    atualização de repositorio

--- aula10/exemplo04.py
+++ aula10/exemplo04.py
@@ -12,7 +12,12 @@ except (ValueError, TypeError):
     print('Informe um valor numérico.')
 except ZeroDivisionError:
     print('Não é possível dividir por zero.')
+except KeyboardInterrupt:
+    print('Operação encerrada pelo usuário.')
 else:
     print(f'Media por funcionário: {media_por_funcionario:.2f}')
+
+# Executa sempre, com erro ou não.
+    
 finally:
     print('Programa encerrado. Reinicie o processo.')
